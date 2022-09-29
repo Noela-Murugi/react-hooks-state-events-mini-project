65daf76fad701688144ee304507d4d55c5824471
Noela-Murugi <murugin254@gmail.com>
2022-09-29 11:09:05 +0300

    completed the assignment

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,26 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, taskDelete}) {
+  // const handleDelete = (id) => {
+  //   const filteredList = list.filter((item, index) => {
+  //     return index !== id;
+  //   });
+  //   setList(filteredList)
+  // }
+  const taskList = tasks.map((task) => (
+    <Task
+      key={task.text}
+      text={task.text}
+      category={task.category}
+      taskDelete={taskDelete}
+    />
+  ));
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {taskList}
+
     </div>
   );
 }
